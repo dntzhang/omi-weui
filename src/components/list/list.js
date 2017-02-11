c14e52f80d160a4ab83165fe571bc13782cf940f
pasturn <alan@pasturn.com>
2017-02-11 19:09:14 +0800

    fix List Class Name

--- src/components/list/list.js
+++ src/components/list/list.js
@@ -1,6 +1,6 @@
 import Omi from 'omi/dist/omi'
 
-export default class Button extends Omi.Component{
+export default class List extends Omi.Component{
     constructor(data) {
         super(data);
     }
